791363dcebe875d0c6f8837027956c6a2e83c5f4
Oleg Dergunov <oleg.dergunov@outlook.com>
2026-03-19 15:59:11 +0000

    Add project files.

---
+++ InspectionTracker.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InspectionTracker.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
